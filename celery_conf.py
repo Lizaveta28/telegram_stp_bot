02d16c07dfc437eba00dbaab2abfd9dc9f77fbbf
nkotov <nkotov@sdvor.com>
2016-11-16 08:39:58 +0500

    small fix

--- celery_conf.py
+++ celery_conf.py
@@ -26,6 +26,7 @@ def check_messages():
     notify_users = Message.select(Message.to_user).distinct().where(Message.is_read == False)
     for message in notify_users:
         if message.to_user.has_messages_after_notification:
-            tb.send_message(message.to_user.telegram_chat_id, "Появились новые сообщения для завки(ок)",
-                            reply_markup=generate_custom_keyboard(telebot.types.InlineKeyboardMarkup, [
-                                [get_button_inline("Мои заявки", 'show_active_requests')]]))
+            user = message.to_user
+            user.has_messages_after_notification = False
+            user.save()
+            tb.send_message(message.to_user.telegram_chat_id, "Появились новые сообщения для завки(ок)")
