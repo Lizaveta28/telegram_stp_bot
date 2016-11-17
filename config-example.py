491e41e2efff4a5ae1dee3e90fc764d55c7ae064
nkotov <nkotov@telegram-bots>
2016-11-17 10:21:17 -0500

    Config example

---
+++ config-example.py
@@ -0,0 +1,14 @@
+token = 'bot-token'
+host = 'db-host'
+password = 'db-pass'
+user = 'db-user'
+db = 'db-name'
+PAGE_SIZE = 'Page size for something pagination'
+EMAIL_HOST = 'email-host' # not necessary
+EMAIL_PORT = 'email-port' # not necessary
+EMAIL_HOST_USER = 'email-host' # not necessary
+EMAIL_USE_SSL = 'True/False' # not necessary
+EMAIL_FROM = SERVER_EMAIL = 'mail from header' # not necessry
+IS_PROD = 'True/False if False - prints some debug info'
+REQUEST_PAGE_SIZE = 'page size for request pagination'
+CHAT_PAGE_SIZE = 'page size for chat messages pagination'
